69a09ffe9b3c9fc912f97bbf8f4778ac58f0aec7
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-07-24 00:12:25 -0600

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import style from './App.module.css'
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <nav className={style.sidebar}>
+      <div className={style.menuBar}>
+        <ul className={style.menuLinks}>
+          <li className={`${style.navLinks}`}>
+            <a href='#'>
+              <span className={`${style.text} ${style.navText}`}>Item 1</span>
+            </a>
+          </li>
+          <li className={`${style.navLinks}`}>
+            <a href='#'>
+              <span className={`${style.text} ${style.navText}`}>Item 2</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
